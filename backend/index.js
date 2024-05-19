c2d24d02612c4e9671b6fbe54c09e8bcd64720ae
ihide14 <juandiman26@gmal.com>
2024-05-19 18:04:30 -0500

    try hope server

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,11 @@ import { app } from './app.js'
 import path from 'path'
 import { PORT } from './utils/config.js'
 import express from 'express'
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {

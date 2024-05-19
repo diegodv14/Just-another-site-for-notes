c2c9b3453c6986938064cf1799aff13b4f2df7f8
ihide14 <juandiman26@gmal.com>
2024-05-19 18:24:45 -0500

    fix send server

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>);
+    </HashRouter>);

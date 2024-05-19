0a3a6c8ff5d78e1796d1e88a8df6f93815e26cb0
ihide14 <juandiman26@gmal.com>
2024-05-19 17:50:03 -0500

    try fix server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import { app } from './app.js'
+import path from 'path'
 import { PORT } from './utils/config.js'
+import express from 'express'
+import { fileURLToPath } from 'url';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

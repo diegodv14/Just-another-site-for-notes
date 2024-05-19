9f3bcf96bbacd2bd84642bd68f417adb92096bd3
ihide14 <juandiman26@gmal.com>
2024-05-19 17:54:53 -0500

    try fix server -2

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,12 @@ import { PORT } from './utils/config.js'
 import express from 'express'
 import { fileURLToPath } from 'url';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
